ca48e6b4470b8258be2e07225beb9c3920c0e807
gugga <faisalbawazir69@gmail.com>
2025-04-25 11:29:29 +0300

    Delete modules directory

--- modules/alerts.py
+++
@@ -1,8 +0,0 @@
-class AlertSystem:
-    def check_congestion(self, people_count):
-        if people_count > 15:
-            return "Alert: Almost Full - Notify Security,", "Take Control"
-        elif people_count > 9:
-            return "Warning: Getting Crowded,"," Keep it Open"
-        else:
-            return "No congestion,"," Keep it Open"
